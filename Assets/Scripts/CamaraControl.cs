027a7879b4dbc5fb5eda9207256c012e78d6068e
darkzilion <darkzilion@hanmail.net>
2021-09-09 17:02:52 +0900

    [Camara] 기본 Camera Panning

---
+++ Assets/Scripts/CamaraControl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraControl : MonoBehaviour
+{
+    public Camera cam;
+
+    private Vector3 dragOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PanCamera();
+    }
+
+    void PanCamera()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
+            cam.transform.position += difference;
+        }
+    }
+}
